c506ad3394725a5f6175de3a7530a3639cb9d0a2
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-11-06 20:38:56 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,19 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Cronometro from "./component/cronometro";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ let counter = 0;
+ setInterval(function(timeMachine){
+    counter++;
+    const six = Math.floor(counter / 100000 % 10);
+    const five = Math.floor(counter / 10000 % 10);
+    const four = Math.floor(counter / 1000 % 10);
+    const three = Math.floor(counter / 100 % 10);
+    const two = Math.floor(counter / 10 % 10);
+    const one = Math.floor(counter / 1 % 10);
+    ReactDOM.render(
+        <Cronometro secOne={one} secTwo={two} secThree={three} secFour={four} secFive={five} secSix={six} />,
+        document.querySelector("#app")
+         );
+     },1000);
